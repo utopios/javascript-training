4167d214ae0ceb4c469d621185838009f9014273
ihab abadi <ihab@utopios.net>
2023-10-10 20:21:51 +0200

    demo with array

--- demo-arrays/main.js
+++ demo-arrays/main.js
@@ -1,56 +1,72 @@
 //Creating array
-let daysOfWeek = ["Monday", "Tuesday"]
+// let daysOfWeek = ["Monday", "Tuesday"]
 
-//console.table(daysOfWeek)
+// //console.table(daysOfWeek)
 
-//Access to the first element
-const firstday = daysOfWeek[0]
+// //Access to the first element
+// const firstday = daysOfWeek[0]
 
-//console.log(firstday)
+// //console.log(firstday)
 
-//Add at the end of array, one item
-daysOfWeek.push("Wednesday")
-daysOfWeek.push("Tuesday")
-
-
-//Add at the end of array, multiple items
-daysOfWeek.push("Thursday", "Friday", "Saturday", "Sunday")
+// //Add at the end of array, one item
+// daysOfWeek.push("Wednesday")
+// daysOfWeek.push("Tuesday")
 
 
-//console.table(daysOfWeek)
-
-//Remove the last item for our array
-daysOfWeek.pop()
-
-daysOfWeek.push(34)
-daysOfWeek.push(["names", 10, false])
+// //Add at the end of array, multiple items
+// daysOfWeek.push("Thursday", "Friday", "Saturday", "Sunday")
 
 
+// //console.table(daysOfWeek)
 
-console.table(daysOfWeek)
+// //Remove the last item for our array
+// daysOfWeek.pop()
 
-const indexTuesday = daysOfWeek.indexOf("Tuesday")
+// daysOfWeek.push(34)
+// daysOfWeek.push(["names", 10, false])
 
-console.log(indexTuesday)
 
 
-const shallowCopy = daysOfWeek.slice()
+// console.table(daysOfWeek)
 
-let copy = [...daysOfWeek]
+// const indexTuesday = daysOfWeek.indexOf("Tuesday")
 
-//iterate over the array
+// console.log(indexTuesday)
 
-for(let i=0; i < daysOfWeek.length; i++) {
-    console.warn(`Index : ${i}, value : ${daysOfWeek[i]}`)
+
+// const shallowCopy = daysOfWeek.slice()
+
+// let copy = [...daysOfWeek]
+
+// //iterate over the array
+
+// for(let i=0; i < daysOfWeek.length; i++) {
+//     console.warn(`Index : ${i}, value : ${daysOfWeek[i]}`)
+// }
+
+// copy.forEach((item, index) => {
+//     console.log(`Index : ${index}, value : ${item}`)
+// })
+
+// copy.pop()
+// copy.pop()
+// const elementsStartedWithT = copy.filter(item => item.startsWith("T"))
+
+// console.debug(elementsStartedWithT)
+
+
+
+let elements = [1,3, 4]
+
+function addElements(originalArray, ...newElements) {
+
+    //newElements.forEach(n => originalArray.push(n))
+
+    originalArray = [...originalArray, ...newElements]
+
+    console.table(originalArray)
 }
 
-copy.forEach((item, index) => {
-    console.log(`Index : ${index}, value : ${item}`)
-})
+addElements(elements, 5,6,7)
 
-copy.pop()
-copy.pop()
-const elementsStartedWithT = copy.filter(item => item.startsWith("T"))
-
-console.debug(elementsStartedWithT)
-
+console.table(elements)
