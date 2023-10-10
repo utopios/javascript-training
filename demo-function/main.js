8b1ad9040b2546e0489f0618b0f9beae740fcea4
ihab abadi <ihab@utopios.net>
2023-10-10 18:51:17 +0200

    demo of function and exercises

--- demo-function/main.js
+++ demo-function/main.js
@@ -1,8 +1,88 @@
 //
 
 
-sayHello()
+// sayHello("ihab", "abadi")
 
-function sayHello() {
-    console.warn("Hello world")
+
+
+// lotOfMessagesAfterES6("m1", "m2")
+
+// lotOfMessagesBeforeES6(["m1", "m2", "m3"])
+
+// computeWithMessage("Addition", 1, 3, 6, 4)
+
+
+// let a = 10
+
+// //sending args by value
+// muliplicationBy3(a)
+
+// //Sending args by reference
+// const person = {firstname : "ihab"}
+
+// makeUpperCase(person)
+
+// console.warn(person)
+
+
+// console.warn(`outside funcation value of a : ${a}`)
+
+
+// function sayHello(firstname, lastname) {
+//     console.warn(`Hello ${firstname} ${lastname}`)
+// }
+
+// //Function with infinity number of args after es6
+// function lotOfMessagesAfterES6(...messages) {
+//     console.log(messages)
+// }
+
+// ////Function with infinity number of args before es6 
+// function lotOfMessagesBeforeES6(messages) {
+//     console.log(messages)
+// }
+
+// function computeWithMessage(message, ...numbers) {
+//     console.log(message)
+//     let total = 0;
+//     for(let i=0; i < numbers.length; i++) {
+//         total += numbers[i]
+//     }
+//     console.log(total)
+// }
+
+
+// function muliplicationBy3(a) {
+//     a = a * 3
+//     console.warn(`Inside function a has value ${a}`)
+// }
+
+// function makeUpperCase(person) {
+//     person = {firstname: "newpersonfirst"}
+//     person.firstname = person.firstname.toUpperCase()
+//     console.warn(person)
+// }
+
+//Anonymous function
+
+//To check
+
+// const message = "Custom message to our anonymous function"
+
+// (function(msg) {
+//   console.warn("Hello i'm anonymous function")  
+//   console.warn(msg)  
+// })("Custom message to our anonymous function")
+
+function compute(a, b, computationMethod) {
+    console.log(computationMethod(a,b))
+    console.log(typeof computationMethod)
 }
+
+const referenceToAnonymousFunction = function(a,b){return a+b}
+
+compute(10,20, referenceToAnonymousFunction)
+
+compute(10,30, function(a,b){return a*b})
+
+compute(10,30, (a,b) =>  a-b)
