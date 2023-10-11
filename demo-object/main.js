ddb42466f3de7b5ea401c7fa4b2786af4dbc64b6
ihab abadi <ihab@utopios.net>
2023-10-11 19:05:18 +0200

    demo with object and first object exercise

--- demo-object/main.js
+++ demo-object/main.js
@@ -4,23 +4,108 @@
 //const myObject = new Object()
 
 
-const person = {
-    firstname : "ihab",
-    lastname: "abadi",
-    age: 36,
-    address : {
-        city: "Tourcoing",
-        zipCode: 59200
-    },
-    printFullname: function() {
-        setTimeout(() => {
-            console.log(this)
-            console.log("Method used to print fullname "+this.firstname + " "+this.lastname)
-        },3000)
-    }
+// const person = {
+//     firstname : "ihab",
+//     lastname: "abadi",
+//     age: 36,
+//     address : {
+//         city: "Tourcoing",
+//         zipCode: 59200
+//     },
+//     // printFullname: function() {
+//     //     console.log(this)
+//     //     setTimeout(function() {
+//     //         console.log(this)
+//     //         console.log("Method used to print fullname "+this.firstname + " "+this.lastname)
+//     //     },3000)
+//     // }
+
+//     printFullname() {
+//         console.log(this.firstname + " "+ this.lastname)
+//     },
+
+//     get fullname() {
+//         return `${this.firstname} ${this.lastname}`
+//     },
+//     set fullname(value) {
+//         [this.firstname, this.lastname] = value.split(' ')
+//     }
+// }
+
+// console.log(person)
+
+// console.log(person.fullname)
+
+// person.fullname = "newfirstname newlastname"
+
+// console.log(person.fullname)
+
+// console.log("firstname" in person)
+// console.log("companyName" in person)
+
+// for(let key in person) {
+//     console.log(`key : ${key}, value: ${person[key]}`)
+// }
+
+//Not work now, 
+// for(let val of person) {
+//     console.log(val)
+// }
+
+//Create object from the first one
+// const secondPerson = Object.create(person)
+// secondPerson.phone = "003311111111"
+// console.log(secondPerson)
+// console.log(secondPerson.firstname)
+
+
+
+// console.log(person.firstname)
+// console.log(person.address)
+
+// person.printFullname()
+
+// function greet(greeting, punctuation) {
+//     console.log(greeting + " "+ this.firstname + " "+ punctuation)
+// }
+
+// greet("Hello", "!")
+
+// const person = {
+//     firstname : "ihab"
+// }
+
+// //Bind function, is function that return new one
+// const greetPerson = greet.bind(person)
+
+// greetPerson("Hello", "!")
+
+// greet.call(person, "Hello", "!")
+
+// greet.apply(person, ["Hello", "!"])
+
+
+//Create object with constructor function
+
+function Person(firstname, lastname) {
+    this.firstname = firstname
+    this.lastname = lastname
+    this.fullname = firstname + " "+lastname
+
+    // this.printname = function() {
+    //     console.log(`hy im ${this.fullname}`)
+    // }
 }
 
-console.log(person.firstname)
-console.log(person.address)
+Person.prototype.printname = function() {
+    console.log(`hy im ${this.fullname}`)
+}
 
-person.printFullname()
+const p1 = new Person("ihab", "abadi")
+const p2 = new Person("toto", "tata")
+
+p1.printname()
+p2.printname()
+
+console.log(p1)
+console.log(p2)
---
+++ exercises/object-exercises.md
@@ -0,0 +1,11 @@
+# Exercise on Objects: Store's Product Management
+
+You're overseeing the management of a store's products. Each product in your inventory has a name, a brand, a category, and an indication of whether it's on sale or not.
+
+**Features to develop:**
+
+- Addition: Write a function that adds a new product to your inventory.
+- Search: Write a function that searches for a product by name, brand, or category.
+- Status: Write a function that modifies the status of a product (on sale/not on sale).
+- Recommendation: Write a function that recommends a product from a specific category that's not on sale.
+- Deletion: Write a function that removes a product from your inventory using the name as a reference
